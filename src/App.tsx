49d03b338535a46460304e42ed9c68c4efe855bb
akmaljonmordayev <akmaljonmordayev@gmail.com>
2026-04-15 14:07:42 +0500

    done

--- src/App.tsx
+++ src/App.tsx
@@ -1,217 +1,188 @@
-import React from 'react'
+import React from "react";
 
 function App() {
-
-
   //typescript data types
 
-   //number
+  //number
 
-    // let a:number = 90
-    // console.log(a);
-    
+  // let a:number = 90
+  // console.log(a);
 
-      // string
-    // const b:string = "90"
+  // string
+  // const b:string = "90"
 
-    // console.log('a');
-    
-    //boolean
+  // console.log('a');
 
-    // const c:boolean = false 
+  //boolean
 
+  // const c:boolean = false
 
-    // const d:null =null
+  // const d:null =null
 
-    // const u:undefined = undefined
-    // console.log(u);
-    // console.log(typeof u);
-    
+  // const u:undefined = undefined
+  // console.log(u);
+  // console.log(typeof u);
 
-    // const w:any = true
-    // console.log(w);
-    
+  // const w:any = true
+  // console.log(w);
 
-    // const arr: number[] = [12, 45, 67, 89,78,]
-    // const b: string[] = ["hello", 'hello']
-    // const c: boolean[] = [true, false, true]
+  // const arr: number[] = [12, 45, 67, 89,78,]
+  // const b: string[] = ["hello", 'hello']
+  // const c: boolean[] = [true, false, true]
 
-    // tuple
+  // tuple
 
-    // const tup: [number, string, number] = [12, "hello", 13] 
+  // const tup: [number, string, number] = [12, "hello", 13]
 
+  //object
 
-    //object
+  // const obj: {name:string, age:number, married:boolean} = {
+  //   name:"Akmal",
+  //   age:12,
+  //   married:false
+  // }
 
-    // const obj: {name:string, age:number, married:boolean} = {
-    //   name:"Akmal",
-    //   age:12,
-    //   married:false
-    // }
+  // type USER = {
+  //   name:string,
+  //   age:number,
+  //   marrdied:boolean,
+  //   studies:any
+  // }
 
-    // type USER = {
-    //   name:string,
-    //   age:number,
-    //   marrdied:boolean,
-    //   studies:any
-    // }
+  // const user:USER = {
+  //   name:'akmal',
+  //   age:899,
+  //   marrdied:false,
+  //   studies:false
+  // }
+  // const olma:USER={
+  //   name:'olma',
+  //   age:10,
+  //   marrdied:true,
+  //   studies:'bor'
+  // }
 
-    // const user:USER = {
-    //   name:'akmal',
-    //   age:899,
-    //   marrdied:false,
-    //   studies:false
-    // }
-    // const olma:USER={
-    //   name:'olma',
-    //   age:10,
-    //   marrdied:true,
-    //   studies:'bor'
-    // }
+  //union
 
-    //union
+  // const t:number | string  = 12
 
-    // const t:number | string  = 12
+  // console.log(t);
+  // console.log(typeof t);
 
-    // console.log(t);
-    // console.log(typeof t);
-    
+  // literal type
 
+  // let v : 'hello' | 'olma' | 'gul' | 'abdulloh'
 
-    // literal type
+  //   v = 'abdulloh'
 
-    // let v : 'hello' | 'olma' | 'gul' | 'abdulloh'
+  //    enum Role {
+  //   ADMIN = "ADMIN",
+  //   USER = "USER",
+  //   GUEST = "GUEST"
+  // }
 
-    //   v = 'abdulloh'
+  // let role:Role = Role.GUEST;
+  // console.log(role);
 
-//    enum Role {
-//   ADMIN = "ADMIN",
-//   USER = "USER",
-//   GUEST = "GUEST"
-// }
+  //unknown
+  // const a:unknown = true
 
-// let role:Role = Role.GUEST;
-// console.log(role);  
+  //functon type
 
-//unknown
-// const a:unknown = true
+  // function N(a:number, b:string):string{
+  //   return a + b
+  // }
 
-//functon type
+  // console.log(N(23, '45'));
 
-// function N(a:number, b:string):string{
-//   return a + b
-// }
+  // function N(a:number, b:string):void{
+  //   console.log(a + b);
+  // }
 
-// console.log(N(23, '45'));
+  // N(23, '45');
 
-// function N(a:number, b:string):void{
-//   console.log(a + b);
-// }
+  // function N(a:number = 20):void{
+  //   console.log(a);
+  // }
 
-// N(23, '45');
+  // N()
 
+  // //funcsiyaga defaul qiymat berish
 
+  // function M(a?:number):void{
+  //   console.log(a);
+  // }
 
-// function N(a:number = 20):void{
-//   console.log(a);
-// }
+  // M()
 
-// N()
+  //optional parameter
 
-// //funcsiyaga defaul qiymat berish
+  // function N(a:number, b:string, c?:boolean):void{
+  //   console.log(a + b);
+  //   console.log(c);
+  // }
 
-// function M(a?:number):void{
-//   console.log(a);
-// }
+  // N(23, '45', true);
 
-// M()
+  // function N(a:number, b:number):number{
+  //   return a + b
+  // }
 
-//optional parameter
+  // console.log(N(23, 45));
 
+  // let a:unknown = 12
 
+  // if(typeof a === 'string'){
+  //   console.log(a.toUpperCase());
+  // }
 
+  // function a(a?:number):void{
+  //   console.log(a);
+  // }
 
+  // // a()
+  // a(12)
 
-// function N(a:number, b:string, c?:boolean):void{
-//   console.log(a + b);
-//   console.log(c);
-// }
+  // function b(a:number = 20):void{
+  //   console.log(a);
+  // }
 
-// N(23, '45', true);
+  // ()
 
+  // let a:never
 
+  // function error(message:string):never{
+  //   throw new Error(message)
+  // }
 
-// function N(a:number, b:number):number{
-//   return a + b
-// }
+  // error('This is an bilol error')
 
-// console.log(N(23, 45));
+  // interface User{
+  //   name:string,
+  //   age:number,
+  //   married:boolean
+  // }
 
-// let a:unknown = 12
+  // interface User{
+  //   studies:any
+  // }
 
-// if(typeof a === 'string'){
-//   console.log(a.toUpperCase());
-// }
+  // //interface larni birlashtirish mumkin
 
+  // const user:User = {
+  //   name:'akmal',
+  //   age:899,
+  //   married:false,
+  //   studies:false
+  // }
 
-// function a(a?:number):void{
-//   console.log(a);
-// }
+  // type User = [string, number, boolean]
+  // type A = string | number | boolean
+  // //interface User2 = [string, number, boolean] //mumkin emas, interface lar faqat object uchun ishlatiladi
 
-// // a()  
-// a(12)
-
-
-// function b(a:number = 20):void{
-//   console.log(a);
-// }
-
-// ()
-
-// let a:never
-
-// function error(message:string):never{
-//   throw new Error(message)
-// } 
-
-// error('This is an bilol error')
-
-
-
-
-// interface User{
-//   name:string,
-//   age:number,
-//   married:boolean
-// }
-
-// interface User{
-//   studies:any
-// }
-
-
-// //interface larni birlashtirish mumkin
-
-
-// const user:User = {
-//   name:'akmal',
-//   age:899,
-//   married:false,
-//   studies:false
-// }
-
-
-// type User = [string, number, boolean]
-// type A = string | number | boolean
-// //interface User2 = [string, number, boolean] //mumkin emas, interface lar faqat object uchun ishlatiladi
-
-// const user:User = ['akmal', 12, false]
-// const user2:User = ['olma', 10, true]
-// const mohinur: A = true
-
- 
-
-
+  // const user:User = ['akmal', 12, false]
+  // const user2:User = ['olma', 10, true]
+  // const mohinur: A = true
 
   // interface Mohinur {
   //   name:string,
@@ -242,14 +213,141 @@ function App() {
   //   job:'developer'
   // }
 
+  // type Mohinur = {
+  //   name: string;
+  //   age: number;
+  //   married: boolean;
+  //   studies: any;
+  // };
+
+  // type Xojar = Mohinur & {
+  //   job: string;
+  // };
+
+  // const xojar: Xojar = {
+  //   name: "xojar",
+  //   age: 12,
+  //   married: false,
+  //   studies: "bor",
+  //   job: "developer",
+  // };
+
+  // interface User {
+  //   name: string;
+  //   age: number;
+  //   married: boolean;
+  //   studies: any;
+  // }
+
+  // interface User2 extends User {
+  //   job: string;
+  // }
+
+  // const user2: User2 = {
+  //   name: "user2",
+  //   age: 12,
+  //   married: false,
+  //   studies: "bor",
+  //   job: "developer",
+  // };
+
+  //utility types
+
+  //Partial
+  //Omit
+  //Pick
+  //Readonly
+  //NonNullable
+  //Required
+
+  //Record
+  //ReturnType
+  //Parameters
+  //ConstructorParameters
+  //InstanceType
+  //Exclude
+  //Extract
+  //Uppercase
+  //Lowercase
+  //Capitalize
+  //Uncapitalize
+
+  // interface User {
+  //   name: string;
+  //   age: number;
+  //   married: boolean;
+  //   studies: any;
+  // }
+
+  // const user: Partial<User> = {
+  //   married: false,
+  //   studies: "bor",
+  // };
+
+  // interface User2 {
+  //   name: string;
+  //   age: number;
+  //   married: boolean;
+  //   studies: any;
+  // }
+  
+  // const user2: Omit<User2, "age" | "married"> = {
+  //   name: "user2",
+  //   studies: "bor",
+  // };
+
+  // const user3: Pick<User2, "name" | "age"> = {
+  //   name: "user3",
+  //   age: 12,
+  // };
+
+  // const user4: Readonly<User2> = {
+  //   name: "user4",
+  //   age: 12,
+  //   married: false,
+  //   studies: "bor",
+  // };
+  
+//   user4.name = "new name"; // Error: Cannot assign to 'name' because it is a read-only property.
+//   delete user4.age; // Error: Cannot delete property 'age' because it is a read-only property.
+//   user4.studies = "new studies"; // Error: Cannot assign to 'studies' because it is a read-only property.
+// console.log(user4);
+
+
+  // type User3 = 12 | null | undefined
+  // let user5: NonNullable<User3> = 12
+  // // user3 = null // Error: Type 'null' is not assignable to type 'NonNullable<User3>'.
+  // // user3 = undefined // Error: Type 'undefined' is not assignable to type 'NonNullable<User3>'.
+  // console.log(user5);
+
+
+  // interface User4 {
+  //   name?: string;
+  //   age?: number;
+  //   married?: boolean;
+  //   studies?: any;
+  // }
+
+  // const user6:Required<User4> = {
+  //   name: "user6",
+  //   age: 12,
+  //   married: false,
+  //   studies: "bor",
+  // };
 
 
 
 
 
-  return (
-    <div>App</div>
-  )
+
+
+
+
+
+
+
+
+  return <div>App</div>;
 }
 
-export default App
+export default App;
